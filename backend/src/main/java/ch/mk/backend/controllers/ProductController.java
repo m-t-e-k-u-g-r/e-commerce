3b860dac6afc09067980229fce83937bf57ec8f8
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-04-02 16:10:33 +0200

    feat: add GET endpoint to fetch one product by ID

--- backend/src/main/java/ch/mk/backend/controllers/ProductController.java
+++ backend/src/main/java/ch/mk/backend/controllers/ProductController.java
@@ -4,6 +4,7 @@ import ch.mk.backend.entities.Product;
 import ch.mk.backend.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,12 @@ public class ProductController {
     private final ProductRepository productRepository;
 
     @GetMapping
-    public List<Product> findAll() {
+    public List<Product> findAllProducts() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProductById(@PathVariable int id) {
+        return productRepository.findById(id).orElse(null);
     }
 }
