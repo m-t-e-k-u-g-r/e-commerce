571a259e154405b8a0215d7ce78b3fe20c8b7f19
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-04-02 15:52:49 +0200

    feat: add ProductController with GET endpoint to fetch all products

---
+++ backend/src/main/java/ch/mk/backend/controllers/ProductController.java
@@ -0,0 +1,23 @@
+package ch.mk.backend.controllers;
+
+import ch.mk.backend.entities.Product;
+import ch.mk.backend.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@AllArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
