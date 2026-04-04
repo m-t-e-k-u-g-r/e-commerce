93100a048a3748ad99622ee76fba0daf9c732ebf
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-04-04 09:19:53 +0200

    feat: enhance CartService with total calculations and quantity management

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -1,31 +1,58 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal, computed } from '@angular/core';
 import { CartItem } from '../models/cartItem.type';
+import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
+  productService = inject(ProductService);
   private _cart = signal<CartItem[]>([]);
   readonly cart = this._cart.asReadonly();
-  readonly totalItems = this.cart().reduce((total, item) => total + item.quantity, 0);
+  readonly totalItems = computed(() =>
+    this.cart().reduce((total, item) => total + item.quantity, 0),
+  );
+  readonly totalAmount = computed(() =>
+    this.cart().reduce((total, item) => {
+      const product = this.productService.products().find((p) => p.id === item.productId);
+      const price = product?.price ?? 0;
+      return total + item.quantity * price;
+    }, 0),
+  );
 
   addItem(id: number) {
-    const item = this.cart().find(item => item.productId === id);
-    if (item) {
-      item.quantity++;
-    } else {
-      this._cart.update(items => [...items, { productId: id, quantity: 1 }]);
-    }
+    this._cart.update((items) => {
+      const index = items.findIndex((i) => i.productId === id);
+      if (index !== -1) {
+        return items.map((i) => (i.productId === id ? { ...i, quantity: i.quantity + 1 } : i));
+      }
+      return [...items, { productId: id, quantity: 1 }];
+    });
     this.saveToLocalStorage();
   }
 
-  editQuantity(id: number, quantity: number) {
-    if (quantity == 0) return;
-    this._cart.update(items => items.map(item => item.productId === id ? { ...item, quantity } : item));
+  reduceQuantity(id: number) {
+    this._cart.update((items) => {
+      const index = items.findIndex((i) => i.productId === id);
+      if (index === -1) return items;
+
+      if (items[index].quantity === 1) {
+        return items.filter((item) => item.productId !== id);
+      } else {
+        return items.map((i) => (i.productId === id ? { ...i, quantity: i.quantity - 1 } : i));
+      }
+    });
+    this.saveToLocalStorage();
   }
 
   removeItem(id: number) {
-    this._cart.update(items => items.filter(item => item.productId !== id));
+    this._cart.update((items) => items.filter((item) => item.productId !== id));
+    this.saveToLocalStorage();
+  }
+
+  clearCart() {
+    this._cart.set([]);
+    this.saveToLocalStorage();
   }
 
   private saveToLocalStorage() {
