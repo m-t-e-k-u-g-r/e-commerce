98fe53d923b4d7798eef7b4c5df617cca171f21e
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-04-05 22:46:09 +0200

    feat: add address dtos and mapper

---
+++ backend/src/main/java/ch/mk/backend/mappers/AddressMapper.java
@@ -0,0 +1,12 @@
+package ch.mk.backend.mappers;
+
+import ch.mk.backend.dtos.AddressDto;
+import ch.mk.backend.entities.Address;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface AddressMapper {
+    @Mapping(source = "user.id", target = "userId")
+    AddressDto toDto(Address address);
+}
