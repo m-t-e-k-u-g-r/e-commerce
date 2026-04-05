ec48da5924a3553edd03920a57888d4a48eb69ee
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-04-05 14:21:49 +0200

    fix: only return product id instead of product in CartItemMapper

--- backend/src/main/java/ch/mk/backend/mappers/CartItemMapper.java
+++ backend/src/main/java/ch/mk/backend/mappers/CartItemMapper.java
@@ -3,8 +3,10 @@ package ch.mk.backend.mappers;
 import ch.mk.backend.dtos.CartItemDto;
 import ch.mk.backend.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartItemMapper {
+    @Mapping(source = "product.id", target = "productId")
     CartItemDto toDto(CartItem cartItem);
 }
