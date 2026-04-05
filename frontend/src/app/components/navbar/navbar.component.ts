f86785155d9d13cb9639898d231c6ec8829b0358
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-04-05 08:59:12 +0200

    feat: implement login functionality with component

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -15,10 +16,16 @@ import { CartService } from '../../services/cart.service';
         </a>
         <p>{{ this.cartService.totalItems() }}</p>
       </div>
+      <div class="user_info">
+        @if (!this.authService.isLoggedIn()) {
+          <a href="/login">Log in</a>
+        }
+      </div>
     </nav>
   `,
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   cartService = inject(CartService);
+  authService = inject(AuthService);
 }
