3a061c5b94880f7866220c1919840067aeaa68fa
m-t-e-k-u-g-r <matteo.krueger@stud.kbw.ch>
2026-04-05 11:50:49 +0200

    feat: add user menu with logout functionality

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { AuthService } from '../../services/auth.service';
+
+export type MenuItem = {
+  label: string;
+  action: () => void;
+};
 
 @Component({
   selector: 'app-navbar',
@@ -19,6 +24,21 @@ import { AuthService } from '../../services/auth.service';
       <div class="user_info">
         @if (!this.authService.isLoggedIn()) {
           <a href="/login">Log in</a>
+        } @else {
+          <div class="user_info">
+            <button (click)="toggle()" class="profile_button">
+              {{ this.authService.userEmail() }}
+            </button>
+            @if (this.menuOpen()) {
+              <ul class="info_menu">
+                @for (item of this.menuItems; track item.label) {
+                  <li (click)="onItemClick(item)">
+                    {{ item.label }}
+                  </li>
+                }
+              </ul>
+            }
+          </div>
         }
       </div>
     </nav>
@@ -28,4 +48,20 @@ import { AuthService } from '../../services/auth.service';
 export class NavbarComponent {
   cartService = inject(CartService);
   authService = inject(AuthService);
+  menuOpen = signal<Boolean>(false);
+
+  onItemClick(item: MenuItem) {
+    item.action();
+  }
+
+  toggle() {
+    this.menuOpen.update(value => !value);
+  }
+
+  menuItems: MenuItem[] = [
+    { label: 'Logout', action: () => {
+      this.authService.logout().subscribe();
+      this.menuOpen.set(false);
+    }},
+  ]
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { catchError, switchMap, tap, throwError } from 'rxjs';
+import { User } from '../models/user.type';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,24 @@ export class AuthService {
   baseUrl = environment.apiUrl + 'auth';
   http = inject(HttpClient);
   isLoggedIn = signal<boolean>(false);
+  userEmail = signal<string | null>(null);
+
+  getUser() {
+    return this.http
+      .get<User>(
+        this.baseUrl + '/me',
+        { withCredentials: true }
+      ).pipe(
+        tap((user: User) => {
+          this.userEmail.set(user.email);
+          this.isLoggedIn.set(true);
+        }),
+        catchError((err) => {
+          this.refresh().subscribe();
+          return throwError(() => err);
+        }),
+      );
+  }
 
   signup(email: string, password: string) {
     return this.http
@@ -68,6 +87,23 @@ export class AuthService {
             default:
               console.error('Failed to refresh token:', err);
           }
+          return throwError(() => err);
+        }),
+      );
+  }
+
+  logout() {
+    return this.http
+      .delete(
+        this.baseUrl + '/logout',
+        { withCredentials: true }
+      ).pipe(
+        tap(() => {
+          this.isLoggedIn.set(false);
+          this.userEmail.set(null);
+        }),
+        catchError((err) => {
+          console.error('Failed to logout:', err);
           return throwError(() => err);
         }),
       );
