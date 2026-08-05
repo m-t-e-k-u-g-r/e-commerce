6a4bfa63ac1aeea9714b696991af843fe6e07a75
m-t-e-k-u-g-r <matteo.krueger.ch@gmail.com>
2026-08-05 19:09:08 +0200

    refactor: move example images to resources

--- backend/src/main/java/ch/mk/backend/controllers/ImageController.java
+++ backend/src/main/java/ch/mk/backend/controllers/ImageController.java
@@ -2,17 +2,14 @@ package ch.mk.backend.controllers;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/images")
@@ -21,15 +18,14 @@ public class ImageController {
 
     @GetMapping("/products/{image_id}")
     public ResponseEntity<Resource> getImageById(@PathVariable String image_id) {
-        Path path = Paths.get("uploads/products/webp/product_" + image_id + ".webp");
+        Resource resource = new ClassPathResource("uploads/products/webp/product_" + image_id + ".webp");
 
-        try {
-            Resource resource = new UrlResource(path.toUri());
-            return ResponseEntity.ok()
-                    .contentType(MediaType.parseMediaType("image/webp"))
-                    .body(resource);
-        } catch (Exception e) {
+        if (!resource.exists()) {
             return ResponseEntity.notFound().build();
         }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("image/webp"))
+                .body(resource);
     }
 }
