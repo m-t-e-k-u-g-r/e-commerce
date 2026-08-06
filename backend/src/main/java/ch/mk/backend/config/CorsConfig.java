cd25b3d443d85cb0d004f76e8da1b64c2ddb1232
m-t-e-k-u-g-r <matteo.krueger.ch@gmail.com>
2026-08-06 10:55:01 +0200

    refactor: introduce profile for CORS configuration

---
+++ backend/src/main/java/ch/mk/backend/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package ch.mk.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    @Profile("development")
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:4200", "http://localhost:4200"));
+        configuration.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE"
+        ));
+        configuration.setAllowedHeaders(List.of(
+                "Content-Type", "Authorization"
+        ));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
