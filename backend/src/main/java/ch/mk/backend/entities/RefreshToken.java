a537c1f6756fdcd23da81b3eaea6c05f2bd3ca01
m-t-e-k-u-g-r <matteo.krueger.ch@gmail.com>
2026-08-16 07:11:02 +0200

    refactor: save token_hash of bytearray as refresh token in DB

--- backend/src/main/java/ch/mk/backend/entities/RefreshToken.java
+++ backend/src/main/java/ch/mk/backend/entities/RefreshToken.java
@@ -21,8 +21,11 @@ public class RefreshToken {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(name = "token", nullable = false)
-    private String token;
+    @Column(name = "token_hash", nullable = false)
+    private byte[] tokenHash;
+
+    @Column(name = "createdAt", nullable = false)
+    private Instant createdAt;
 
     @Column(name = "expires_at", nullable = false)
     private Instant expiresAt;
